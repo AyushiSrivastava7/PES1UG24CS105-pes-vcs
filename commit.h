1feb0db0f72b52bd40bda8a7a3199e44b77d527c
AyushiSrivastava7 <ayusri170422@gmail.com>
2026-04-18 17:52:55 +0530

    Update commit.h

--- commit.h
+++ commit.h
@@ -2,6 +2,7 @@
 #define COMMIT_H
 
 #include "pes.h"
+#include <time.h>
 
 typedef struct {
     ObjectID tree;
@@ -12,7 +13,6 @@ typedef struct {
     char message[4096];
 } Commit;
 
-// commit API
 int commit_create(const char *message, ObjectID *commit_id_out);
 int commit_parse(const void *data, size_t len, Commit *commit_out);
 int commit_serialize(const Commit *commit, void **data_out, size_t *len_out);
@@ -20,7 +20,6 @@ int commit_serialize(const Commit *commit, void **data_out, size_t *len_out);
 typedef void (*commit_walk_fn)(const ObjectID *id, const Commit *commit, void *ctx);
 int commit_walk(commit_walk_fn callback, void *ctx);
 
-// HEAD helpers
 int head_read(ObjectID *id_out);
 int head_update(const ObjectID *new_commit);
 
