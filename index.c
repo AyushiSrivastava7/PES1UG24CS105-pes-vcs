ecc7440a684ed6aa14599c6d643c2391aa9c0e07
AyushiSrivastava7 <ayusri170422@gmail.com>
2026-04-19 17:34:27 +0530

    Phase 3: load index

--- index.c
+++ index.c
@@ -6,7 +6,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-// external API
+// external AP
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out);
 
 // ───────────────────────────────
